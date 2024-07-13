a0052e28aed9e8e89e310d8ad32fc45da85615dc
Irnam Soimansoib <irnam.sa@gmail.com>
2024-07-13 16:14:09 +0200

    Add cookie parser and json middlewares to server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const app = express()
 const path = require('path')
+const cookieParser = require("cookie-parser")
 const PORT = process.env.PORT || 3500
+
+
+
+app.use(express.json())
+
+app.use(cookieParser())
 
 app.use ('/', express.static(path.join(__dirname,'public')))
 
