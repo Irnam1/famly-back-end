3c54a03d10ecea58e61819648206233b2d9b4467
Irnam Soimansoib <irnam.sa@gmail.com>
2024-08-13 13:08:35 +0200

    Create user controller for '/users' routes

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'editor', 'user'], default: 'user' },
-    familyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Family', required: true }
+    roles: [{ type: String, enum: ['admin', 'editor', 'user'], default: 'user' }],
+    familyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Family', required: false }
 })
 
 module.exports = mongoose.model('User', userSchema);
